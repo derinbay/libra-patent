147d7ff7838d15c07a874dabfd28b02e68183ade
Taylan Derinbay <taylan.derinbay@n11.com>
2018-09-25 21:06:18 +0300

    add cografi-isaret

--- cografi-isaret.php
+++ cografi-isaret.php
@@ -2,6 +2,52 @@
 include 'header.php';
 ?>
 
+<!--================Home Banner Area =================-->
+<section class="banner_area">
+    <div class="banner_inner d-flex align-items-center">
+        <div class="container">
+            <div class="banner_content text-center">
+                <div class="page_link">
+                    <a href="index.php">Ana Sayfa</a>
+                    <a href="cografi-isaret.php">Coğrafi İşaret ve Geleneksel Ürün Adı</a>
+                </div>
+                <h2>Coğrafi İşaret ve Geleneksel Ürün Adı</h2>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================End Home Banner Area =================-->
+
+<!--================Home Gallery Area =================-->
+<section class="home_gallery_area p_120">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Coğrafi işaret belirgin bir niteliği, ünü veya diğer özellikleri bakımından kökeninin bulunduğu yöre, alan, bölge veya ülke ile özdeşleşmiş bir ürünü gösteren işarettir.</p>    
+                <p>Coğrafi işaretler 2 ‘ye ayrılır;</p>
+                <ul>
+                    <li>Menşe Adı</li>
+                    <li>Mahreç Adı</li>
+                </ul>
+                <p>Menşe Adı: Coğrafi sınırları belirlenmiş bir yöre, bölge veya istisnai durumlarda ülkeden kaynaklanan, tüm veya esas özelliklerini bu coğrafi alana özgü doğal ve beşeri unsurlardan alan, üretimi, işlenmesi ve diğer işlemlerin tümü bu coğrafi alanın sınırları içinde gerçekleşen ürünleri tanımlayan adlar Menşe adıdır. Menşe adları sadece ilgili coğrafi alanda üretilirler.</p>
+                <p>Mahreç işareti ise belirli bir coğrafi alanla özdeşleşen ve üretimi, işlenmesi ve diğer işlemlerinden en az biri belirlenmiş coğrafi sınırın içinde yapılan ürünleri tanımlayan adlardır. Mahreç işaretlerde ürün özelliklerinden en az biri o yöreye aittir ancak bölge dışında da üretimi gerçekleştirilebilmektedir.</p>
+                <p>Geleneksel ürün adı;</p>
+                <p>Diğer taraftan menşe adı ve mahreç işareti kapsamına girmeyen ve ilgili piyasada bir ürünü tarif etmek için geleneksel olarak en az otuz yıl süreyle kullanıldığı kanıtlanan adlar geleneksel ürün adı olarak korunurlar.</p>
+                <p>Ancak bu ürünlerin;</p>
+                <ul>
+                    <li>geleneksel üretim veya işleme yöntemi  yahut geleneksel bileşimden kaynaklanması ya da</li>
+                    <li>geleneksel hammadde veya malzemelerle üretilmiş olması gerekmektedir.</li>
+                </ul>
+                <p>Örneğin baklava, lokum,  pastırma gibi ürünler Türk Patent ve Marka Kurumu nezrinde tescil işlemleri tamamlanmış Coğrafi işaret ve geleneksel ürün adlarında, üreticilerin ürettikleri ürün ambalajlarında Türk Patent ve Marka Kurumu tarafından hazırlanan ambalajları kullanma zorunluluğu bulunmaktadır ve ambalaj kullanımı denetime tabidir.</p>
+                <h3 class="text-heading title_color">Coğrafi işaret / Geleneksel Ürün Adı Tescili</h3>
+                <p>Coğrafi işaret ve geleneksel ürün adları belli bir bölgede yaşayan ve belirlenmiş kriterlerde üretim yapan bölgedeki tüm üreticilere ait haklar olduğundan, bu hakların tescili aynı şekilde tüm üreticilere menfaat sağlamaktadır.</p>
+                <p>Belirli bir kalitenin sembolü haline gelmiş geleneksel ürünlerin ticari değerlere dönüştürülmesi için tescil ve sonrasındaki yasal gerekliliklerin tamamlanması gerekmektedir.</p>
+                <p>Ürünün ekonomik değerinin yükselmesi pazarlama imkanının çoğaltılması ve neticesinde bölgesel kalkınmaya da yardımcı olmaktadır.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php
 include 'footer.php';
 ?>
